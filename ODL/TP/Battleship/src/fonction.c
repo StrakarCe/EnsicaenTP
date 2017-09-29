985365b4b9e54d00c6170091d2897f1d16bc2880
Anendil <duvalflorian14@gmail.com>
2017-09-29 12:22:04 +0200

    Ajout des entetes

---
+++ ODL/TP/Battleship/src/fonction.c
@@ -0,0 +1,16 @@
+/*
+ * ENSICAEN
+ * 6 Boulevard Marechal Juin
+ * F-14050 Caen Cedex
+ *
+ * This file is owned by ENSICAEN students.
+ * No portion of this document may be reproduced, copied
+ * or revised without written permission of the authors.
+ */
+ /**
+  * @author Florian DUVAL <florian.duval@ecole.ensicaen.fr>
+  * @version 0.0.1 – 2017-09-29
+  *
+  * @todo the list of improvements suggested for the file.
+  * @bug the list of known bugs.
+  */
